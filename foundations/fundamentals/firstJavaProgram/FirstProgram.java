c9d6ef992f33b7d3a03efa9e6f1901a9572a84ce
czmud <caleb.zmuda@protonmail.com>
2022-07-30 14:18:44 -0700

    first commit

---
+++ foundations/fundamentals/firstJavaProgram/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+	public static void main(String[] args) {
+        System.out.println("My name is czmud");
+        System.out.println("I am older than 16 and younger than 80");
+        System.out.println("My home town is pigsleyville, OH");
+	}
+}
