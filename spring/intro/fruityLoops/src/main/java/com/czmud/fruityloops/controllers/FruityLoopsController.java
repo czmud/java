c29ac85ac3e315f9a3ef6eaccebbe7d933f12dfa
czmud <caleb.zmuda@protonmail.com>
2022-08-06 18:11:15 -0700

    upload fruity loops

---
+++ spring/intro/fruityLoops/src/main/java/com/czmud/fruityloops/controllers/FruityLoopsController.java
@@ -0,0 +1,26 @@
+package com.czmud.fruityloops.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.czmud.fruityloops.Item;
+
+import org.springframework.ui.Model;
+import java.util.ArrayList;
+
+@Controller
+public class FruityLoopsController {
+	@RequestMapping("/")
+	public String index(Model model) {
+		
+		ArrayList<Item> fruits = new ArrayList<Item>();
+	    fruits.add(new Item("Kiwi", 1.5));
+	    fruits.add(new Item("Mango", 2.0));
+	    fruits.add(new Item("Goji Berries", 4.0));
+	    fruits.add(new Item("Guava", .75));
+	    
+	    model.addAttribute("fruits", fruits);
+		
+		return "index.jsp";
+	}
+}
