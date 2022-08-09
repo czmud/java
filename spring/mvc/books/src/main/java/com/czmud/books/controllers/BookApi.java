dba52b966de80961ce1b74ba34f6e636753069ea
czmud <caleb.zmuda@protonmail.com>
2022-08-09 15:49:46 -0700

    upload books

---
+++ spring/mvc/books/src/main/java/com/czmud/books/controllers/BookApi.java
@@ -0,0 +1,43 @@
+package com.czmud.books.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.czmud.books.models.Book;
+import com.czmud.books.services.BookService;
+
+@RestController
+public class BookApi {
+	private final BookService bookService;
+	
+	public BookApi(BookService bookService){
+		this.bookService = bookService;
+	}
+	@RequestMapping("/")
+	public String index() {
+		return "howard";
+	}
+	
+	// other methods removed for brevity
+	@RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+	public Book update(
+			@PathVariable("id") Long id,
+			@RequestParam(value="title") String title, 
+			@RequestParam(value="description") String desc, 
+			@RequestParam(value="language") String lang,
+			@RequestParam(value="pages") Integer numOfPages) {
+		Book book = new Book(id, title, desc, lang, numOfPages);
+		return bookService.updateBook(book);
+	}
+	
+	@RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+	public void destroy(@PathVariable("id") Long id) {
+		bookService.deleteBook(id);
+	}
+}
+
+
+
---
+++ spring/mvc/books/src/main/java/com/czmud/books/services/BookService.java
@@ -0,0 +1,49 @@
+package com.czmud.books.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.czmud.books.models.Book;
+import com.czmud.books.repositories.BookRepository;
+
+@Service
+public class BookService {
+    // adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    
+    // creates a book
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+    
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    
+    // updates a book
+    public Book updateBook( Book book) {
+    	return bookRepository.save(book);
+    }
+    
+    // deletes a book
+    public void deleteBook(Long id) {
+    	bookRepository.deleteById( id );
+    }
+}
