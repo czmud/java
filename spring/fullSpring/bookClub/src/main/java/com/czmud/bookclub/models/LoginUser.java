613d84595db8f2f23cde7982bfdad2250d75bfb2
czmud <caleb.zmuda@protonmail.com>
2022-08-16 14:36:38 -0700

    cleaning up dependencies

--- spring/fullSpring/bookClub/src/main/java/com/czmud/bookclub/models/LoginUser.java
+++ spring/fullSpring/bookClub/src/main/java/com/czmud/bookclub/models/LoginUser.java
@@ -2,7 +2,6 @@ package com.czmud.bookclub.models;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 public class LoginUser {
 	
